cc4360bda2065e000eb17242a1268b3a8b940933
Matheus Silva <matheusuniversal@yahoo.com.br>
2020-03-24 20:01:55 -0300

    Back-end da Aplicacao

---
+++ backend/src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+/*
+* Rota / Recurso
+*/
+
+/*
+Metodos HTTP:
+Get: Buscar um informacao do back-end
+Post: Criar uma informacao no back-end
+PUT: Alterar uma informacao no back-end
+DELETE: Deletar uma informacao no back-end
+*/
+
+
+/*
+Tipos de parametros
+Query: Parametros nomeados na rota apos "?""(filtros, paginacao e etc)
+Route Params: Parametros  utilizados para identificar recursos
+Request Body : Corpo da requisicao , utilizado para criar ou alterar recursos
+*/
+
+/*
+SQL : Mysql, SQLite, PostGree, Oracle sql, SQL server
+NoSQL: MongoDB, CouchDB, etcnpm install knex
+*/
+
+app.listen(3333);
