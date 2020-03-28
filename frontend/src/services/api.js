a425b58601acf679ab1c7ee9bd18f3ef2cefaa03
Matheus Silva <matheusuniversal@yahoo.com.br>
2020-03-28 11:17:37 -0300

    add server back api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    //baseURL: 'http://localhost:3333',
+    baseURL: 'http://31.220.48.30:3333',
 })
 
 export default api;
